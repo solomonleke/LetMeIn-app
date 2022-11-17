6ea6e7299ac68c11e889737d30eb8cf412451db0
Onwurah121 <obinnaedmund121@gmail.com>
2022-11-17 23:36:04 +0100

    Super Admin App under construction

---
+++ src/Pages/superAdmin/NewOffice.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainLayout from '../../Layouts/Index'
+import Seo from '../../Utils/Seo'
+
+export default function NewOffice() {
+  return (
+    <MainLayout>
+   <Seo title='LetMeIn SuperAdmin Dashboard' description='LetMeIn SuperAdmin Dashboard'/>
+   </MainLayout>
+  )
+}
